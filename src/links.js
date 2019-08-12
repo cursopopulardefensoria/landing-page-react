ae926b564cfe02e0ec1e36d1193ff8132dcbd7b9
Rafael Santos <rafael.santos.193@gmail.com>
2019-08-12 07:59:59 -0300

    feat: Update Vakinha link

--- src/links.js
+++ src/links.js
@@ -1,6 +1,6 @@
 export default {
     aulasOnline: "http://env1.cursopopulardefensoria.com.br/",
-    vakinha: "https://www.vakinha.com.br/vaquinha/3-curso-popular-de-formacao-de-defensoras-e-defensores-publicos",
+    vakinha: "https://www.vakinha.com.br/vaquinha/4-curso-popular-de-formacao-de-defensoras-e-defensores-publicos",
     youtube: "https://www.youtube.com/channel/UCW8LLCnZrmVxwSIno-7AI4A",
     instagram: "https://www.instagram.com/cursopopulardefensoria/",
     facebook: "https://www.facebook.com/cursopopulardefensoria/",
